847fc8ca1b9f2251c623fa353c4b3e86be44533c
Satyabrata Pradhan <spradhan143as@gmail.com>
2025-12-28 15:34:12 +0100

    changes for training

--- object_detection/train.py
+++ object_detection/train.py
@@ -2,17 +2,17 @@ from ultralytics import YOLO
 
 def train():
     # Load a model
-    model = YOLO('yolov8l.pt')  # load an official model
+    model = YOLO('yolo11m.pt')  # load an official model (will auto-download if not present)
 
     # train the model
     model.train(data='object_detection/door_detector.yaml', 
                 device=-1,
                 epochs=100, imgsz=640, batch=32, pretrained=True,
                 workers=4, lr0=0.01, patience=100, save_period=10, seed=42,
-                name='yolov8l_door_detector', resume=False, save=True)
+                name='yolo11m_door_detector', resume=False, save=True)
     
 
 if __name__ == "__main__":
     from clearml import Task
-    task = Task.init(project_name="robotdog_guide", task_name="train_door_detector_yolov8l")
+    task = Task.init(project_name="robotdog_guide", task_name="train_door_detector_yolo11m")
     train()
